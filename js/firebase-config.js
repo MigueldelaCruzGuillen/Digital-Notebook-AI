02159deb42cb011f80d30c83bba7d842691adeb0
Miguel de la Cruz Guillén <diosanthony4@gmail.com>
2026-06-09 14:37:08 -0400

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAGksAsBH2_YDy1OaQA71w6UyzCyxUkwjA",
+  authDomain: "mi-cuaderno-b4c93.firebaseapp.com",
+  projectId: "mi-cuaderno-b4c93",
+  storageBucket: "mi-cuaderno-b4c93.firebasestorage.app",
+  messagingSenderId: "422820967447",
+  appId: "1:422820967447:web:90500b89e9804f6ea91852",
+};

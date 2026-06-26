a7db275cb9ad728ee20d25f76353ff3e19b54b5a
Miguel de la Cruz Guillén <diosanthony4@gmail.com>
2026-06-26 14:07:13 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,17 @@
-export const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: "AIzaSyAGksAsBH2_YDy1OaQA71w6UyzCyxUkwjA",
   authDomain: "mi-cuaderno-b4c93.firebaseapp.com",
   projectId: "mi-cuaderno-b4c93",
   storageBucket: "mi-cuaderno-b4c93.firebasestorage.app",
   messagingSenderId: "422820967447",
-  appId: "1:422820967447:web:90500b89e9804f6ea91852",
+  appId: "1:422820967447:web:90500b89e9804f6ea91852"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

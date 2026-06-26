5aba4bac60aa62cb0841ba887dff547a328c2e18
Miguel de la Cruz Guillén <diosanthony4@gmail.com>
2026-06-26 14:35:52 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAGksAsBH2_YDy1OaQA71w6UyzCyxUkwjA",
-  authDomain: "mi-cuaderno-b4c93.firebaseapp.com",
-  projectId: "mi-cuaderno-b4c93",
-  storageBucket: "mi-cuaderno-b4c93.firebasestorage.app",
-  messagingSenderId: "422820967447",
-  appId: "1:422820967447:web:90500b89e9804f6ea91852"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+{
+  "type": "service_account",
+  "project_id": "mi-cuaderno-b4c93",
+  "private_key_id": "42b1bd481f3d890935d97e7524a0b33ed1cb1c1a",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC7OEOFRgQVObWM\n8GLIvlwUqPMyuV44DcnSxO0YKNCBDBWMWxrMz9YwgkYK91OS5WDELUbprqqGy1eZ\n0iqSEt1Mdg8WbxJKOA79YNff3k5Cp0p1cYG5t0LmgRkytqFE6Qy2g2fqmt5y5D7i\nrHZNAe6lGukX4nOs9Ywt+u2uP21Iscl31nL4T5k1Vp6ry5fKueoPI1Tg29SigbVc\n7fsltIL6ypWok6D5+thukWzIKOdQWm0ISg3Ro8fPl9l4g3qpZCYsSR5H5FEgAQK/\nA41zh0WME3AeaBIiTPS8ke5NKlml4a7s92bBLQ7Q5JsseHg56c84EzYRmDNaOC0a\noeq/EVKjAgMBAAECggEAFdFtQPtTX3vdtgKPqJBSn8++o9MpcUPeGNN9Q/sBrYBQ\nhI5bdbHk5rtP2VhQY4aau9IA+RuzYMkSuOjesC5FnvMFOBqSmZal3I5sKfk4ypi/\nWydytEdwovc5jgjti1/TbPOsaRFKSj5iO71YfgnzG0ejEXcA/w+Qmg0rpMpcfIOU\ni49UoIK5rT1jGI1lA1DWrQUBBiaBTxn/HMQg4tEJ2/oxFPJo8R8K7GO1vtnxltdJ\nra9DS34N71+azkNz5YdneoP4Fu/jT+Fpo9XikgPsBH/HWd+UPB+K9vdVHWz+Ztdn\n3Jv1/qQrNGPgQ+qlSk+YAYdIBy4l1H/aNG9h3byL0QKBgQDe6oyDD27OBlfZCTsc\n7wTmhud/Fz8ryIRghNd5rCZb0CUf/yL3wwu0nta5FRx9yCzwYyN+5d5/9uoVIA1s\nLm+dEByed79yGG0DywnzRIEgZ56MQiJVEr6pmjbpBBV6jYt00Wn38qMJzvHUfYia\nNS6LzkWgORBUz8waQd3KkHTZ0wKBgQDXAXgerHttLmPFDOt/gqLDWS8c/jxLWOGw\nH7FbjNTFcSJ7m1h/Zhh73h8E7iLFcqETbjLPYsjE37/6e50eDOcgt+duIpFL1ua4\nPJwFWMFUcKsg6O2Y3tOw9s38y+spvzvGGTZME+8zh55jI6+fGuh4N9Vo+29Ia/bV\nrpi1b/nR8QKBgEbz6R2eP+qb4kJWydX/zlOCBRVS1OyeumPuZJd50AvRSL/SedLa\n5lLra/UlXxg6KvOo2pd9m0Cn2wI2XY9diHE5u6JFA07Rr4FRky03VNJ3qOOD+WGS\nwdVCHNA2WI/Vldns/gnJ1VM954N3IY/T9ajSUx8YirY0HgcNiAb9y/O5AoGBALYz\nxomn/YjO0TnB8yhBmpEVWPmm8Y2gqtFIalJ9DCqt71z0jpCYApOl7n+NFuZgd+Qx\ndNmOizlbrCMm8kTvBkcR0gvhO2Qbn7XtlwJo7p0SRdND0WXFH3xOCs36OTVNm+gK\noZrjIch3W2AEc+XPNcX6+t1Whj3HUIxQw5SuafThAoGAYiCttWoPsONdFwSVLc4f\nMPNRs1Od4speZguXVny/JEVuYWikxpPdCBavL4Cl2Twsb+LIf7Gzhll4cdg3jGl+\nHwPee5plTh6SWWa623/WkenueLQbEQgHJtZP+CQgZmfZI2It7hgij2ih1QPfegTb\n6a+9WTztWz8jpsfW1JCAxQ4=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@mi-cuaderno-b4c93.iam.gserviceaccount.com",
+  "client_id": "112699940801325401478",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40mi-cuaderno-b4c93.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"

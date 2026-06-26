6c8c1137f346bea0ac5330188569afa45fc01143
Miguel de la Cruz Guillén <diosanthony4@gmail.com>
2026-06-26 14:01:01 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAbF111pCQ_8NeFI034wGFwQvsauFrk_Nc",
+  apiKey: "AIzaSyAGksAsBH2_YDy1OaQA71w6UyzCyxUkwjA",
   authDomain: "mi-cuaderno-b4c93.firebaseapp.com",
   projectId: "mi-cuaderno-b4c93",
   storageBucket: "mi-cuaderno-b4c93.firebasestorage.app",
